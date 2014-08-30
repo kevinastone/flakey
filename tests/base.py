eb43fb9862f641b322b06d975e7cc226d9d09e17
Kevin Stone <kevinastone@gmail.com>
2014-08-29 18:59:50 -0700

    Fixed python3 issue in opening files

--- tests/base.py
+++ tests/base.py
@@ -10,7 +10,7 @@ except ImportError:
 class BaseCheckerTestCase(unittest.TestCase):
     def _get_fixture(self, filename):
         fixture_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'fixtures', filename)
-        return file(fixture_path)
+        return open(fixture_path)
 
     def get_tree(self, fixture):
         return ast.parse(self._get_fixture(fixture).read())
